0030532343744b59d38114760002e3a114965b52
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-09-26 21:38:42 +0600

    test(http3): cover frame-owned encoding

--- tests/Http3FrameTest.php
+++ tests/Http3FrameTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Infocyph\Runwire\Http\Http3\Enum\FrameType;
 use Infocyph\Runwire\Http\Http3\Frame;
 use Infocyph\Runwire\Http\Http3\FrameParser;
-use Infocyph\Runwire\Http\Http3\FrameWriter;
 use Infocyph\Runwire\Http\Http3\VarIntCodec;
 
 it('round-trips every QUIC variable integer width', function (int $value): void {
@@ -15,7 +14,7 @@ it('round-trips every QUIC variable integer width', function (int $value): void 
 })->with([0, 63, 64, 16_383, 16_384, 1_073_741_823, 1_073_741_824, VarIntCodec::MAX_VALUE]);
 
 it('parses HTTP/3 frames incrementally across arbitrary stream splits', function (): void {
-    $wire = FrameWriter::encode(new Frame(FrameType::HEADERS->value, 'header-block')) . FrameWriter::encode(new Frame(FrameType::DATA->value, 'body'));
+    $wire = (new Frame(FrameType::HEADERS->value, 'header-block'))->encode() . (new Frame(FrameType::DATA->value, 'body'))->encode();
     $parser = new FrameParser();
     $frames = [];
     foreach (str_split($wire, 2) as $chunk) { array_push($frames, ...$parser->push($chunk)); }
